d22fd74cb8e7170b1c018a24bb042a2e182b1253
Mathias Dalheimer <dalheime@lts016.itwm.fhg.de>
2010-09-29 10:57:52 +0200

    added license terms

--- libflukso/flukso.hpp
+++ libflukso/flukso.hpp
@@ -1,3 +1,23 @@
+/**
+ * This file is part of libflukso.
+ *
+ * (c) Mathias Dalheimer <md@gonium.net>, 2010
+ *
+ * libflukso is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * libflukso is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libflukso. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef LIBFLUKSO_FLUKSO_HPP
 #define LIBFLUKSO_FLUKSO_HPP 1
 
@@ -23,7 +43,8 @@ namespace Flukso {
       Webservice (const Webservice& original);
       Webservice& operator= (const Webservice& rhs);
       void run_query(std::string* buffer) throw(Flukso::CommunicationException);
-      TimeseriesPtr parse_json_data(const char* inputData) throw (Flukso::DataFormatException);
+      TimeseriesPtr parse_json_data(const char* inputData) 
+        throw (Flukso::DataFormatException);
 
       Config::Ptr _config;
   };
--- src/flukso-getvalues.cpp
+++ src/flukso-getvalues.cpp
@@ -1,10 +1,29 @@
+/**
+ * This file is part of libflukso.
+ *
+ * (c) Mathias Dalheimer <md@gonium.net>, 2010
+ *
+ * libflukso is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * libflukso is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libflukso. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <common.hpp>
 #include <flukso.hpp>
 #include <config.hpp>
-#include <exporter-text.hpp>
-#include <exporter-xml.hpp>
 #include <exporter.hpp>
 #include <iostream>
+#include <fstream>
 
 int main (int argc, char const* argv[]) {
   Flukso::Config::Ptr config(Flukso::Config::buildConfigFromCmdLine(argc, argv));
@@ -12,7 +31,14 @@ int main (int argc, char const* argv[]) {
 	Flukso::Webservice::Ptr webservice(new Flukso::Webservice(config));
 	Flukso::TimeseriesPtr ts(webservice->get_values());
 	Flukso::Exporter::Ptr exporter = Flukso::Exporter::buildExporter(config, ts);
-	std::cout << *exporter << std::endl;
+	if (true) {
+	  std::cout << *exporter << std::endl;
+	} else {
+	  std::ofstream outfile;
+	  outfile.open ("test.txt", std::ofstream::out | std::ofstream::app);
+	  outfile << *exporter;
+	  outfile.close();
+	}
   } catch (Flukso::GenericException ge) {
 	std::cout << "Failed to retrieve values, reason:" << std::endl 
 	  << "  " << ge.reason() << std::endl;
