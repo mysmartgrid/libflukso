b0b688e411148d1e9fb125dc74d4df56753942a1
Mathias Dalheimer <md@gonium.net>
2010-10-22 15:34:01 +0200

    moved chumby damon to msg-chumby-display repo

--- chumby/daemonconfig.lua
+++
@@ -1,8 +0,0 @@
-config = {
-  IP      = "192.168.1.102",
-  SENSOR  = "a0e9cbec251ee1ef53310ccd64032d6a",
-  DATADIR = "/Users/md/tmp/flukso",
-  BINPATH = "/Users/md/Projects/mysmartgrid/libflukso.git",
-  CMD     = "/build/src/flukso-getvalues"
-}
-
